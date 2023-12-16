74cd3741aecfd12a797bdcf0c3258f0864fec3ae
Ophirmdhn <ophiramadhan18@gmail.com>
2023-12-16 12:04:46 +0700

    Membuat profile screen

--- app/src/main/java/com/ophi/animeapp/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/ophi/animeapp/ui/screen/profile/ProfileScreen.kt
@@ -1,23 +1,61 @@
 package com.ophi.animeapp.ui.screen.profile
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ophi.animeapp.R
+import com.ophi.animeapp.ui.theme.AnimeAppTheme
 
 
 @Composable
 fun ProfileScreen(
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier. fillMaxSize()
     ) {
-        Text(stringResource(R.string.menu_profile))
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "Profile",
+            modifier = Modifier
+                .padding(top = 32.dp, bottom = 16.dp)
+                .size(200.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(R.string.ophi),
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = FontWeight.Bold
+            )
+        )
+        Text(
+            text = stringResource(R.string.email_ophi),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun ProfilePreview() {
+    AnimeAppTheme {
+        ProfileScreen()
     }
 }
